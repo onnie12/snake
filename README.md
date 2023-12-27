d50337a3f7dc339070961836b8c2bd8f514c226f
Onni Johansson <152686116+onnie12@users.noreply.github.com>
2023-12-27 19:22:28 +0200

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # snake
 snake game
+
+
+## Todo
+
+- [ ] Add game logo to html page
+- [ ] crash logic
+- [ ] fruit: increase score, make snake longer
+- [ ] press space to start
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 
-const app = new PIXI.Application({ background: '#1099bb', resizeTo: window });
+const app = new PIXI.Application({ background: '#1099bb', width: 500, height: 500  });
 
-document.body.appendChild(app.view);
+document.getElementById("pixi-container").appendChild(app.view);
 
 const container = new PIXI.Container();
 
@@ -10,24 +10,53 @@ app.stage.addChild(container);
 document.addEventListener('keydown', onKeyDown);
 
 // Create a new texture
-const texture = PIXI.Texture.from('https://pixijs.com/assets/bunny.png');
+const texture_head = PIXI.Texture.from('snake_head.png');
+const texture_body = PIXI.Texture.from('snake_body.png');
+const texture_apple = PIXI.Texture.from('apple_image.png');
+const texture_background = PIXI.Texture.from('tile_aqua.png');
 
-const sprite = new PIXI.Sprite(texture);
+
+const sprite_body = new PIXI.Sprite(texture_body);
+const sprite_head = new PIXI.Sprite(texture_head);
+const sprite_apple = new PIXI.Sprite(texture_apple);
+
+//container.addChild(sprite_body)
 // Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+//container.x = 500;
+//container.y = 500;
 
 // Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
+//container.pivot.x = 100;
+//container.pivot.y = 100;
 
-
+const tilingSprite = new PIXI.TilingSprite(
+    texture_background,
+    app.screen.width,
+    app.screen.height,
+);
+container.addChild(tilingSprite);
 let x=10; 
 let y=10;
 let direction = "left"
+let apple_x = 250;
+let apple_y = 250;
+sprite_apple.x = apple_x
+sprite_apple.y = apple_y
+container.addChild(sprite_apple)
+container.addChild(sprite_head)
+function testForAABB(object1, object2)
+{
+    const bounds1 = object1.getBounds();
+    const bounds2 = object2.getBounds();
+
+    return bounds1.x < bounds2.x + 25
+        && bounds1.x + 25 > bounds2.x
+        && bounds1.y < bounds2.y + 25
+        && bounds1.y + 25 > bounds2.y;
+}
+
 
 function onKeyDown(key) {
-    console.log(key);
     // W Key is 87
     // Up arrow is 87
     if (key.keyCode === 87 || key.keyCode === 38) {
@@ -76,47 +105,70 @@ let snake=[]
 let length=5;
 
 function addToSnake(x,y,snake){
-    const tmp = new PIXI.Sprite(texture);    
+    const tmp = new PIXI.Sprite(texture_body);    
     container.addChild(tmp)
+    container.addChild(sprite_head)
     tmp.position.x = x;
     tmp.position.y = y;
     snake.unshift(tmp);
-    //debugger;
-    console.log(snake.length,snake.length>length )
      if(snake.length>length){
         let old = snake.pop()
-        console.log(old)
         container.removeChild(old)
      }
 
      
 }
-let step=3;
+let step=10;
+let speed=60;
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-container.addChild(sprite)
+let points = 0;
+let text_child;
+function update_score() {
+    if (text_child) container.removeChild(text_child)
+    text_child = container.addChild(
+        new PIXI.Text("Points: "+points, {
+          fontSize: 24,
+          lineHeight: 28,
+          letterSpacing: 0,
+          fill: 0xffffff,
+          align: "center"
+        })
+      );
+}
 
 // Listen for animate update
 app.ticker.add((delta) =>
 {    
-    sleepFor(60)
-    if (direction=="up" && y>-100){
+    sleepFor(speed)
+    if (direction=="up" && y>0){
         y= y-step;
         addToSnake(x,y,snake);
     }
-    if (direction=="down" && y<200){
+    if (direction=="down" && y<460){
         y= y+step;
         addToSnake(x,y,snake);    }
     if (direction=="left" && x>0){
         x= x-step;
         addToSnake(x,y,snake);
     }
-    if (direction=="right" && x<200){
+    if (direction=="right" && x<460){
         x= x+step;
         addToSnake(x,y,snake);
-        console.log(snake)
     }
-   sprite.position.x = x;
-   sprite.position.y = y;
+
+   sprite_head.position.x = x;
+   sprite_head.position.y = y;
    // container.rotation -= 0.01 * delta;
+   const collison = testForAABB(sprite_head, sprite_apple)
+   if (collison) {
+    sprite_apple.x = getRandomInt(0,460)
+    sprite_apple.y = getRandomInt(0,460)
+    length = length + 1
+    points = points+1
+    update_score() 
+   }
 });
 app.ticker.speed = .1;
