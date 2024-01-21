5d606a1c019f7a8d326c364c513cddfdd88cd813
Onni <152686116+onnie12@users.noreply.github.com>
2024-01-21 16:14:09 +0100

    explosion

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 
+const explosionTextures = [];
+
+
+
 
 const app = new PIXI.Application({ background: '#1099bb', width: 500, height: 500  });
 
 document.getElementById("pixi-container").appendChild(app.view);
 
 const container = new PIXI.Container();
+PIXI.Assets.load('https://pixijs.com/assets/spritesheet/mc.json').then(() =>
+{
+    let i;
 
+    for (i = 0; i < 26; i++)
+    {
+        const texture = PIXI.Texture.from(`Explosion_Sequence_A ${i + 1}.png`);
+
+        explosionTextures.push(texture);
+    }
+});
 app.stage.addChild(container);
 document.addEventListener('keydown', onKeyDown);
 // Create a new texture
@@ -19,6 +33,12 @@ const sprite_body = new PIXI.Sprite(texture_body);
 const sprite_head = new PIXI.Sprite(texture_head);
 const sprite_apple = new PIXI.Sprite(texture_apple);
 
+sprite_head.anchor.x = 0.5;
+sprite_head.anchor.y = 0.5;
+sprite_body.anchor.x = 0.5;
+sprite_body.anchor.y = 0.5;
+sprite_apple.anchor.x = 0.5;
+sprite_apple.anchor.y = 0.5;
 //container.addChild(sprite_body)
 // Move container to the center
 //container.x = 500;
@@ -59,16 +79,30 @@ function testForAABB(object1, object2)
         && bounds1.y + 25 > bounds2.y;
 }
 function end_game(){
-    x=50; 
-    y=50;
+
     direction = "right"
     apple_x = 250;
     apple_y = 250;
     length=5;
     game_started = false
     points = 0;
+    sprite_head.rotation =  0;
+    const explosion = new PIXI.AnimatedSprite(explosionTextures);
+    explosion.x = x
+    explosion.y = y
+    explosion.anchor.set(0.5);
+    explosion.scale.set(0.75 + Math.random() * 0.5);
+    explosion.loop = false
+    explosion.play();
+    app.stage.addChild(explosion);
+    explosion.onComplete = () => {
+        app.stage.removeChild(explosion);
+    };
+
+    x=50; 
+    y=50;
     if (snake.length >0) {
-        for (let body_part in snake) {
+        for (let body_part of snake) {
             container.removeChild(body_part)
         }
     }
@@ -128,6 +162,8 @@ function sleepFor(sleepDuration){
 
 function addToSnake(x,y,snake){
     const tmp = new PIXI.Sprite(texture_body);    
+    tmp.anchor.x = 0.5;
+    tmp.anchor.y = 0.5;
     container.addChild(tmp)
     container.addChild(sprite_head)
     tmp.position.x = x;
@@ -186,19 +222,25 @@ app.ticker.add((delta) =>
         if (direction=="up" && y>0){
             y= y-step;
             addToSnake(x,y,snake);
+            sprite_head.rotation = Math.PI * 2 * 0.0;
         }
-        if (direction=="down" && y<460){
+        if (direction=="down" && y<490){
             y= y+step;
-            addToSnake(x,y,snake);    }
+            addToSnake(x,y,snake);    
+            sprite_head.rotation = Math.PI * 2 * 0.5;
+        }
+            
         if (direction=="left" && x>0){
             x= x-step;
             addToSnake(x,y,snake);
+            sprite_head.rotation = Math.PI * 2 * 0.75;
         }
-        if (direction=="right" && x<460){
+        if (direction=="right" && x<490){
             x= x+step;
             addToSnake(x,y,snake);
+            sprite_head.rotation = Math.PI * 2 * 0.25;
         }
-        if (y >= 459 || y <= 0 || x >= 459 || x <= 0){
+        if (y >= 490 || y <= 0 || x >= 490 || x <= 0){
             end_game()
         }
 
@@ -207,8 +249,8 @@ app.ticker.add((delta) =>
         // container.rotation -= 0.01 * delta;
         const collison = testForAABB(sprite_head, sprite_apple)
         if (collison) {
-            sprite_apple.x = getRandomInt(0,460)
-            sprite_apple.y = getRandomInt(0,460)
+            sprite_apple.x = getRandomInt(10,490)
+            sprite_apple.y = getRandomInt(10,490)
             length = length + 1
             points = points+1
             update_score() 
@@ -220,3 +262,4 @@ app.ticker.add((delta) =>
 
 });
 app.ticker.speed = .1;
+app.start();
